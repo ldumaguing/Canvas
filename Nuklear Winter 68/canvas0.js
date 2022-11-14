480a42a85612708b41e0ff75da6e03029768adab
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-11-14 17:17:58 -0500

    Add files via upload

--- Nuklear Winter 68/canvas0.js
+++ Nuklear Winter 68/canvas0.js
@@ -3,16 +3,12 @@ window.onload = function() {
     let ctx = c.getContext("2d");
     let img = document.getElementById("Map0");
     ctx.drawImage(img,0,0);
-    foo();
+    foo(c, ctx, img);
  }
 
-function foo() {
-    let c = document.getElementById("Canvas0");
-    let ctx = c.getContext("2d");
-    let img;
-
+function foo(c, ctx, img) {
     img = document.getElementById("img30");
-    ctx.drawImage(img,10,10);
+    ctx.drawImage(img,293,141);
     img = document.getElementById("img40");
-    ctx.drawImage(img,50,50);
+    ctx.drawImage(img, 293 + (4896/29), 141 + (3665/38));
 }

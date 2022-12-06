ea978879f5abdf186e08ee9b446960cd98b51fc4
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-12-06 16:32:36 -0500

    Add files via upload

--- Stalingrad 42/codes/src/main.rs
+++ Stalingrad 42/codes/src/main.rs
@@ -2,11 +2,9 @@ use mysql::*;
 use mysql::prelude::*;
 
 #[derive(Debug, PartialEq, Eq)]
-struct Chit {
+struct Image {
     id: i32,
     name: Option<String>,
-    front: Option<String>,
-    back: Option<String>,
 }
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
@@ -17,34 +15,39 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
     let selected = conn
         .query_map(
-            "SELECT id, name, front, back from chits where chitType = 4 order by name",
-            |(id, name, front, back)| {
-                Chit { id, name, front, back }
+            "SELECT id, name from images where imageType = 1 order by name",
+            |(id, name)| {
+                Image { id, name }
             },
     )?;
 
     println!("<!DOCTYPE html>");
     println!("<html><head></head><body><table>");
-    println!("<tr><th>Id</th><th>Front</th>");
-    println!("<th>Back</th><th>Name</th></tr>");
-
-
+    println!("<tr><th>Id</th><th>Image</th>");
+    // println!("<th>Back</th><th>Name</th></tr>");
+    println!("<th>Name</th></tr>");
 
     for chit in selected {
         println!("<tr>");
         println!("<td>{}</td>", chit.id);
         println!("<td><img src=\"/opt/Vassal/Stalingrad 42/{}\" ></td>",
-            chit.front.as_ref().unwrap().to_string() );
+            chit.name.as_ref().unwrap().to_string() );
+        println!("<td>{}</td>",
+            chit.name.as_ref().unwrap().to_string() );
+        /*
         match chit.back.as_ref() {
             None => println!("<td></td>"),
             Some(x) => println!("<td><img src=\"/opt/Vassal/Stalingrad 42/{}\" ></td>", x.to_string()),
         }
-        println!("<td>{}</td>", chit.name.as_ref().unwrap().to_string());
+        */
+        // println!("<td>{}</td>", chit.name.as_ref().unwrap().to_string());
         println!("</tr>");
 	}
 
-    println!("<tr><th>Id</th><th>Front</th><th>Back</th>");
-    println!("<th>Name</th></tr></table></body></html>");
+    // println!("<tr><th>Id</th><th>Front</th><th>Back</th>");
+    // println!("<th>Name</th></tr></table></body></html>");
+    
+    println!("</table></body></html>");
     
     Ok (())
 }

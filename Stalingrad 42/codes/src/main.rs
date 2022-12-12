18e7254055500954ec58c09b0d0e625b626e07d2
Lawrence Dumaguing <os2nlinux@gmail.com>
2022-12-11 20:40:14 -0500

    2022.12a

--- Stalingrad 42/codes/src/main.rs
+++ Stalingrad 42/codes/src/main.rs
@@ -2,11 +2,10 @@ use mysql::*;
 use mysql::prelude::*;
 
 #[derive(Debug, PartialEq, Eq)]
-struct Chit {
+struct Image {
     id: i32,
     name: Option<String>,
-    front: Option<String>,
-    back: Option<String>,
+    unit_id: Option<String>,
 }
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
@@ -17,34 +16,45 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
     let selected = conn
         .query_map(
-            "SELECT id, name, front, back from chits where chitType = 1 order by name",
-            |(id, name, front, back)| {
-                Chit { id, name, front, back }
+            "SELECT id, name, UnitID from images where pieceType = 'combat' order by name",
+            |(id, name, unit_id)| {
+                Image { id, name, unit_id }
             },
     )?;
 
     println!("<!DOCTYPE html>");
     println!("<html><head></head><body><table>");
-    println!("<tr><th>Id</th><th>Front</th>");
-    println!("<th>Back</th><th>Name</th></tr>");
-
-
+    println!("<tr><th>Id</th><th>Image</th>");
+    println!("<th>Unit ID</th>");
+    println!("<th>Name</th></tr>");
+    println!("</tr>");
 
     for chit in selected {
         println!("<tr>");
+        
         println!("<td>{}</td>", chit.id);
+        
         println!("<td><img src=\"/opt/Vassal/Stalingrad 42/{}\" ></td>",
-            chit.front.as_ref().unwrap().to_string() );
-        match chit.back.as_ref() {
+            chit.name.as_ref().unwrap().to_string() );
+        
+        match chit.unit_id.as_ref() {
             None => println!("<td></td>"),
-            Some(x) => println!("<td><img src=\"/opt/Vassal/Stalingrad 42/{}\" ></td>", x.to_string()),
-        }
-        println!("<td>{}</td>", chit.name.as_ref().unwrap().to_string());
+            Some(x) => println!("<td>{}</td>", x.to_string()),
+        }        
+
+        println!("<td>{}</td>",
+            chit.name.as_ref().unwrap().to_string() );
+
+
+
+        // println!("<td>{}</td>", chit.name.as_ref().unwrap().to_string());
         println!("</tr>");
 	}
 
-    println!("<tr><th>Id</th><th>Front</th><th>Back</th>");
-    println!("<th>Name</th></tr></table></body></html>");
+    // println!("<tr><th>Id</th><th>Front</th><th>Back</th>");
+    // println!("<th>Name</th></tr></table></body></html>");
+    
+    println!("</table></body></html>");
     
     Ok (())
 }

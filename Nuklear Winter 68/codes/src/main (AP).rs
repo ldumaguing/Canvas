570c92f431647d80bec1d85c1c43e087f04c377c
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-12-14 13:03:56 -0500

    Add files via upload

--- Nuklear Winter 68/codes/src/main (AP).rs
+++ Nuklear Winter 68/codes/src/main (AP).rs
@@ -12,7 +12,7 @@ struct Image {
 }
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let url = "mysql://ayeka@localhost:3306/Nuklear_Winter_68";
+    let url = "mysql://ayeka:sasamichan@fish1:3306/Nuklear_Winter_68";
     let pool = Pool::new(url)?;
 
     let mut conn = pool.get_conn()?;
@@ -26,12 +26,14 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     )?;
 
     println!("<!DOCTYPE html>");
-    println!("<html><head></head><body><table>");
+    println!("<html><head>");
+    println!("<link rel=\"stylesheet\" href=\"styles.css\">");
+    println!("</head><body><table>");
     println!("<tr><th>Id</th><th>Image</th>");
-    println!("<th>Fire_Red</th>");
-    println!("<th>Fire_Blu</th>");
-    println!("<th>Rang_Red</th>");
-    println!("<th>Rang_Wht</th>");
+    println!("<th></th>");
+    println!("<th></th>");
+    println!("<th></th>");
+    println!("<th></th>");
     // println!("<th>Back</th><th>Name</th></tr>");
     println!("<th>Name</th></tr>");
 
@@ -47,11 +49,21 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
             Some(x) => println!("<td>{}</td>", x.to_string()),
         }
 */
+        if chit.val1 > 0 { println!("<td class=\"Red_font\"><b>{}</b></td>", chit.val1); }
+        else { println!("<td>---</td>"); }
 
-        println!("<td>{}</td>", chit.val1);
-        println!("<td>{}</td>", chit.val2);
-        println!("<td>{}</td>", chit.val3);
-        println!("<td>{}</td>", chit.val4);
+        if chit.val2 > 0 { println!("<td class=\"Blue_font\"><b>{}</b></td>", chit.val2); }
+        else { println!("<td>---</td>"); }
+
+        if chit.val3 > 0 { println!("<td class=\"Red_font\"><b>{}</b></td>", chit.val3); }
+        else { println!("<td>---</td>"); }
+
+        if chit.val4 > 0 { println!("<td class=\"White_font\"><b>{}</b></td>", chit.val4); }
+        else { println!("<td>---</td>"); }
+
+        // println!("<td class=\"Blue_font\">{}</td>", chit.val2);
+        // println!("<td>{}</td>", chit.val3);
+        // println!("<td>{}</td>", chit.val4);
 
         println!("<td>{}</td>",
             chit.name.as_ref().unwrap().to_string() );

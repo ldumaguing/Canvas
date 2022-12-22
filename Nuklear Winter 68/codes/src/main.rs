a2aeaae74d81be8a3f76e28f60599ff71b7f8503
ldumaguing <65552206+ldumaguing@users.noreply.github.com>
2022-12-22 17:27:46 -0500

    Add files via upload

--- Nuklear Winter 68/codes/src/main.rs
+++ Nuklear Winter 68/codes/src/main.rs
@@ -1,5 +1,6 @@
 use mysql::*;
 use mysql::prelude::*;
+use regex::Regex;
 
 #[derive(Debug, PartialEq, Eq)]
 struct Image {
@@ -28,7 +29,6 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     for chit in selected {
 		match chit.location.as_ref() {
 			None => println!("-"),
-			// Some(x) => println!("{}", x.to_string()),
 			Some(x) => {
 				x_loc = get_x(x);
 				y_loc = get_y(x);
@@ -42,12 +42,29 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 }
 
 fn get_x(s: &str) -> i32 {
+	let mut counter = 0;
+	let letters = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+	    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+	    "AA", "BB", "CC", "DD", "EE" ];
+	let bbb: String = String::from(s);
+	for fish in &letters {
+		println!("-------------> {} {}", bbb, fish.eq(&bbb));
+		// println!("..{}", fish.eq(&bbb));
+		if fish.eq(&s) {println!("breaking..."); break;}
+		counter += 1;
+	}
 	println!("... {}", s);
-	5
+	let re = Regex::new(r"[0-9]").unwrap();
+    let after = re.replace_all(s, "");
+	println!("X: {}", after);
+    println!("count: {}", counter);
+	counter
 }
 
 fn get_y(s: &str) -> i32 {
-	println!("... {}", s);
+	let re = Regex::new(r"[a-zA-Z]").unwrap();
+    let after = re.replace_all(s, "");
+	println!("Y: {}", after);
 	6
 }
 

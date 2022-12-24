5f6a9616765bc438a2ab67d9808bf98649aa9239
Lawrence Dumaguing <os2nlinux@gmail.com>
2022-12-23 21:09:24 -0500

    2022.12b

--- Nuklear Winter 68/codes/src/main.rs
+++ Nuklear Winter 68/codes/src/main.rs
@@ -8,7 +8,7 @@ struct Image {
 }
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let url = "mysql://ayeka:sasamichan@10.60.28.124:3306/Nuklear_Winter_68";
+    let url = "mysql://ayeka@localhost:3306/Nuklear_Winter_68";
     let pool = Pool::new(url)?;
 
     let mut conn = pool.get_conn()?;
@@ -21,51 +21,49 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
             },
     )?;
 
-    let mut x_loc = 0;
-    let mut y_loc = 0;
-    
-    println!("1: {}, {}", x_loc, y_loc);
+    let mut x_loc: i32;
+    let mut y_loc: i32;
 
     for chit in selected {
 		match chit.location.as_ref() {
 			None => println!("-"),
-			Some(x) => {
-				x_loc = get_x(x);
-				y_loc = get_y(x);
+			Some(hex) => {
+				x_loc = get_x(hex);
+				y_loc = get_y(hex);
+				println!("{} {}, {}", hex, x_loc, y_loc);
 				},
 		}
 	}
     
-    println!("2: {}, {}", x_loc, y_loc);
+    
     
     Ok (())
 }
 
 fn get_x(s: &str) -> i32 {
+	let letters = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+	                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+	                "U", "V", "W", "X", "Y", "Z",
+	                "AA", "BB", "CC", "DD", "EE" ];
+
 	let mut counter = 0;
-	let letters = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-	    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-	    "AA", "BB", "CC", "DD", "EE" ];
-	let bbb: String = String::from(s);
+	let re = Regex::new(r"[0-9]").unwrap();
+    let x_slice = re.replace_all(s, "");
+	
 	for fish in &letters {
-		println!("-------------> {} {}", bbb, fish.eq(&bbb));
-		// println!("..{}", fish.eq(&bbb));
-		if fish.eq(&s) {println!("breaking..."); break;}
+		if fish.eq(&x_slice) { break; }
 		counter += 1;
 	}
-	println!("... {}", s);
-	let re = Regex::new(r"[0-9]").unwrap();
-    let after = re.replace_all(s, "");
-	println!("X: {}", after);
-    println!("count: {}", counter);
+
 	counter
 }
 
 fn get_y(s: &str) -> i32 {
 	let re = Regex::new(r"[a-zA-Z]").unwrap();
     let after = re.replace_all(s, "");
-	println!("Y: {}", after);
-	6
+	let y = after.parse::<i32>().unwrap();
+
+	y
 }
 
 
